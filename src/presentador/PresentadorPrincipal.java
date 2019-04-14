0457c782894e4550b73fd350989a8d89a135eddd
Cesar <Cesar@Cesar-CoreI5>
2019-04-14 13:33:04 -0300

    Agregue Mensajes de confirmacion para cambio de estado de botones ventana principal

--- src/presentador/PresentadorPrincipal.java
+++ src/presentador/PresentadorPrincipal.java
@@ -8,6 +8,7 @@ package presentador;
 import datos.RepositorioPaquete;
 import interfaz.IPrincipal;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import modelo.Estado;
 import modelo.Paquete;
 import vista.VPrincipal;
@@ -29,15 +30,24 @@ public class PresentadorPrincipal {
     }
 
     public void activarPaquete() {
+        
         int index=vista.getIndexSeleccionado();
         Object valor = vista.getPaqueteSelecionado(index);
         int nroPaquete=(int)valor;
         Paquete paquete = RepositorioPaquete.buscarById(nroPaquete);
         if(paquete.getEstado().equals(Estado.Cancelado)){
             //Advertir que no se puede cambiar
+            JOptionPane.showMessageDialog(null,"No se puede realizar el cambio de estado una vez cancelado");
         }else{
+            //Preguntar confirmacion antes de activar
+            int resp = JOptionPane.showConfirmDialog(null,"¿Desea activar el paquete?","Mensaje",JOptionPane.YES_NO_OPTION);
+            if(resp==JOptionPane.YES_OPTION){
             RepositorioPaquete.setEstadoById(nroPaquete,Estado.Activo);
+            }else{
+                
+            }
         }
+        
         actualizarPaquetes();
     }
     
@@ -48,19 +58,33 @@ public class PresentadorPrincipal {
         Paquete paquete = RepositorioPaquete.buscarById(nroPaquete);
         if(paquete.getEstado()==Estado.Cancelado){
             //Advertir que no se puede cambiar
+             JOptionPane.showMessageDialog(null,"No se puede realizar el cambio de estado una vez cancelado");
         }else{
+            //Preguntar confirmacion antes de desactivar
+            int resp = JOptionPane.showConfirmDialog(null,"¿Desea desactivar el paquete?","Mensaje",JOptionPane.YES_NO_OPTION);
+            if(resp==JOptionPane.YES_OPTION){
             RepositorioPaquete.setEstadoById(nroPaquete,Estado.Inactivo);
+            }
+            else{
+            }
         }
+        
         actualizarPaquetes();
     }
 
     public void cancelarPaquete() {
-        int index=vista.getIndexSeleccionado();
+        //Preguntar confirmacion antes de cancelar
+        int resp = JOptionPane.showConfirmDialog(null,"¿Desea cancelar el paquete?","Mensaje",JOptionPane.YES_NO_OPTION);
+        if(resp==JOptionPane.YES_OPTION){
+         int index=vista.getIndexSeleccionado();
         Object valor = vista.getPaqueteSelecionado(index);
         int nroPaquete=(int)valor;
         Paquete paquete = RepositorioPaquete.buscarById(nroPaquete);
         RepositorioPaquete.setEstadoById(nroPaquete,Estado.Cancelado);
-        actualizarPaquetes();
+        actualizarPaquetes();   
+        }
+        else{
+        }
     }
 
     
